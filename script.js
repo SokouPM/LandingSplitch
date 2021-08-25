fe80a0639b5ce67c0e9dd0ea84fd366336ed87e3
Pierre Marquet <marquet_pierre@yahoo.fr>
2021-08-25 17:48:21 +0200

    update

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 $(document).scroll(function () {
-    console.log($(document).scrollTop());
+    //console.log($(document).scrollTop());
 });
 
+// Forms toggle visible
+$('#meetButton').click(function (e) {
+    e.preventDefault();
+    $('#meetForm').toggleClass('none');
+    $(this).parent().toggleClass('none');
+    setTimeout(function(){
+        $('#meetForm>*').toggleClass('max');
+    },5); 
+});
+
+$('#testButton').click(function (e) {
+    e.preventDefault();
+    $('#testForm').toggleClass('none');
+    $(this).parent().toggleClass('none');
+    setTimeout(function(){
+        $('#testForm>*').toggleClass('max');
+    },5); 
+});
 
 // Tags toggle style
-$(".lineTags").delegate('p', 'click', function () {
+$('.lineTags').delegate('p', 'click', function () {
     $(this).toggleClass('selectedTag');
 });
