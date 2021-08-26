85ac9db1f008b0bba12dfb10a592c15d6dabf688
Pierre Marquet <marquet_pierre@yahoo.fr>
2021-08-26 15:56:06 +0200

    update

--- script.js
+++ script.js
@@ -1,27 +1,97 @@
-$(document).scroll(function () {
-    //console.log($(document).scrollTop());
-});
-
-// Forms toggle visible
+//////////// Forms toggle visible ////////////
 $('#meetButton').click(function (e) {
     e.preventDefault();
     $('#meetForm').toggleClass('none');
     $(this).parent().toggleClass('none');
-    setTimeout(function(){
+    setTimeout(function () {
         $('#meetForm>*').toggleClass('max');
-    },5); 
+    }, 5);
 });
 
 $('#testButton').click(function (e) {
     e.preventDefault();
     $('#testForm').toggleClass('none');
     $(this).parent().toggleClass('none');
-    setTimeout(function(){
+    setTimeout(function () {
         $('#testForm>*').toggleClass('max');
-    },5); 
+    }, 5);
 });
 
-// Tags toggle style
+//////////// Tags toggle style ////////////
 $('.lineTags').delegate('p', 'click', function () {
     $(this).toggleClass('selectedTag');
 });
+
+//////////// Phone1 toggle anim + play ////////////
+$(document).scroll(function () {
+
+    const screenHeight = $(window).innerHeight();
+    let reveal = $('#phoneVideo')[0].getBoundingClientRect().bottom;
+
+    if (reveal < screenHeight / 4 || reveal > screenHeight * 1.5) {
+        $('#phoneVideo').removeClass('phone1OnScreen');
+        $('#phoneVideo').addClass('phone1NotOnScreen');
+        setTimeout(function () {
+            $('#phoneVideo>video').trigger('pause');
+        }, 1000);
+    } else {
+        $('#phoneVideo').removeClass('phone1NotOnScreen');
+        $('#phoneVideo').addClass('phone1OnScreen');
+        setTimeout(function () {
+            $('#phoneVideo>video').trigger('play');
+        }, 1000);
+    }
+});
+
+//////////// Phone2 toggle anim + play ////////////
+$(document).scroll(function () {
+
+    const screenHeight = $(window).innerHeight();
+    let reveal = $('#phoneVideo2')[0].getBoundingClientRect().bottom;
+
+    if (reveal < screenHeight / 4 || reveal > screenHeight * 1.5) {
+        $('#phoneVideo2').removeClass('phone2OnScreen');
+        $('#phoneVideo2').addClass('phone2NotOnScreen');
+        setTimeout(function () {
+            $('#phoneVideo2>video').trigger('pause');
+        }, 1000);
+    } else {
+        $('#phoneVideo2').removeClass('phone2NotOnScreen');
+        $('#phoneVideo2').addClass('phone2OnScreen');
+        setTimeout(function () {
+            $('#phoneVideo2>video').trigger('play');
+        }, 1000);
+    }
+});
+
+//////////// Connect messages anim ////////////
+$(document).scroll(function () {
+
+    let screenPositionTop = $(document).scrollTop();
+    let screenPositionBottom = $(document).scrollTop() + $(window).innerHeight();
+
+
+    //let reveal = $('#messages').position().top;
+
+    console.log('ScrollTop : ' + screenPositionTop);
+    console.log('ScrollBottom : ' + screenPositionBottom);
+
+    // const screenHeight = $(window).innerHeight();
+    // let reveal = $('#messages')[0].getBoundingClientRect().top;
+    // let messTab = $('#messages>*');
+
+    // if (reveal < screenHeight + (screenHeight / 10)) {
+    //     messTab.each(function (i) {
+    //         setTimeout(function () {
+    //             console.log("ok")
+    //             $(this).addClass("messOnScreen");
+    //             $(this).removeClass("messNotOnScreen");
+    //         }, 500);
+    //     });
+    // } else {
+
+    // }
+})
+// console.log($(window).innerHeight());
+// console.log($('#phoneVideo').position().bottom);
+// console.log($('#phoneVideo').height());
